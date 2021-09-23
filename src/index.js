04eb1aa09c0422f1413e59a55e0237fc85d1b34d
Luis Guillermo Gómez Galenao <luisgaleano002@gmail.com>
2021-09-23 14:48:53 -0500

    Se elimina el modo estricto de React (React.StrictMode)

--- src/index.js
+++ src/index.js
@@ -6,12 +6,7 @@ import './index.css';
 import AppRouter from './routes/AppRouter';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
